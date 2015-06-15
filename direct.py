5d6ebcbb28d0f66f1ad858ecec2e79036ab0e63d
NinjaTrench <info@securitylabs.in>
2015-06-15 22:37:57 +0530

    handling remote ical url, debian summit and ubuntu events

---
+++ direct.py
@@ -0,0 +1,35 @@
+__author__ = 'harsh'
+import requests
+from icalendar import Calendar
+from conf import debconf_url, ubuntu_events_url
+from exceptions import InvalidRemoteUrl
+
+
+class DirectUrl(object):
+    def __init__(self, url=None):
+        if url:
+            self.url = url
+        else:
+            raise InvalidRemoteUrl()
+
+    def main(self):
+        r = requests.get(self.url)
+        obj = Calendar.from_ical(r.text)
+        data = obj.walk()
+        return data[1:]
+
+
+class DebSummit(DirectUrl):
+    def __init__(self, url=None):
+        if not url:
+            super().__init__(url=debconf_url)
+        else:
+            super().__init__(url=url)
+
+
+class UbuntuEvents(DirectUrl):
+    def __init__(self, url=None):
+        if not url:
+            super().__init__(url=ubuntu_events_url)
+        else:
+            super().__init__(url=url)
